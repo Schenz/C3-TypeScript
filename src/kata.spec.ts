b75976c92c9b819588c892a301e09d0a185147b7
Brandon Schenz <schenz.bs@pg.com>
2023-01-23 10:05:43 -0500

    20230118-mod-power

--- src/kata.spec.ts
+++ src/kata.spec.ts
@@ -1,4 +1,3 @@
-import { beforeEach, expect, test } from '@jest/globals';
 import { Kata } from './kata';
 
 let fixture: Kata;
@@ -7,7 +6,16 @@ beforeEach(() => {
     fixture = new Kata();
 });
 
-test('hello should return message with specified name', () => {
-    const name = 'Ingage';
-    expect(fixture.hello(name)).toEqual(`Hello ${name}`);
+describe('modPower tests', () => {
+    test.each`
+        a        | b        | n         | expected
+        ${4}     | ${13}    | ${497}    | ${445}
+        ${67930} | ${32319} | ${103969} | ${6582}
+        ${321}   | ${12345} | ${54321}  | ${45816}
+    `(
+        'returns $expected when $a is raised to the $b power then mod $n',
+        ({ a, b, n, expected }) => {
+            expect(fixture.modPower(a, b, n)).toBe(expected);
+        }
+    );
 });
--- src/kata.ts
+++ src/kata.ts
@@ -1,5 +1,13 @@
 export class Kata {
-    public hello(name: string): string {
-        return `Hello ${name}`;
+    public modPower(a: number, b: number, n: number): number {
+        let c = 1,
+            d = 0;
+
+        while (d < b) {
+            d++;
+            c = (a * c) % n;
+        }
+
+        return c;
     }
 }
