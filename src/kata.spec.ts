2e9d923931e8b9f158c8551d94ae5dea5127f415
Brandon Schenz <schenz.bs@pg.com>
2023-02-13 10:47:06 -0500

    round robin solutions

--- src/kata.spec.ts
+++ src/kata.spec.ts
@@ -8,13 +8,34 @@ beforeEach(() => {
 
 describe('Kata Tests', () => {
     it.each`
-        name         | expected
-        ${'Ingage'}  | ${'Hello Ingage'}
-        ${'Brandon'} | ${'Hello Brandon'}
+        teams | expected
+        ${0}  | ${[[-1]]}
+        ${1}  | ${[[-1]]}
+        ${2}  | ${[[1, 2]]}
+        ${3}  | ${[[1, 2], [1, 3], [2, 3]]}
+        ${4}  | ${[[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]}
+        ${10} | ${[[1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7], [1, 8], [1, 9], [1, 10], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7], [2, 8], [2, 9], [2, 10], [3, 4], [3, 5], [3, 6], [3, 7], [3, 8], [3, 9], [3, 10], [4, 5], [4, 6], [4, 7], [4, 8], [4, 9], [4, 10], [5, 6], [5, 7], [5, 8], [5, 9], [5, 10], [6, 7], [6, 8], [6, 9], [6, 10], [7, 8], [7, 9], [7, 10], [8, 9], [8, 10], [9, 10]]}
     `(
-        'returns $expected when $name is passed to function',
-        ({ name, expected }) => {
-            expect(fixture.hello(name)).toEqual(expected);
+        'returns $expected when $teams is passed to function',
+        ({ teams, expected }) => {
+            expect(fixture.roundRobinTournamentRecursive(teams)).toEqual(
+                expected
+            );
+        }
+    );
+
+    it.each`
+        teams | expected
+        ${0}  | ${[]}
+        ${1}  | ${[]}
+        ${2}  | ${[['Team 1', 'Team 2']]}
+        ${3}  | ${[['Team 1', 'Team 3'], ['Team 2', 'Team 2'], ['Team 1', 'Team 2'], ['Team 3', 'Team 3']]}
+        ${4}  | ${[['Team 1', 'Team 4'], ['Team 2', 'Team 3'], ['Team 1', 'Team 3'], ['Team 4', 'Team 2'], ['Team 1', 'Team 2'], ['Team 3', 'Team 4']]}
+        ${10} | ${[['Team 1', 'Team 10'], ['Team 2', 'Team 9'], ['Team 3', 'Team 8'], ['Team 4', 'Team 7'], ['Team 5', 'Team 6'], ['Team 1', 'Team 9'], ['Team 10', 'Team 8'], ['Team 2', 'Team 7'], ['Team 3', 'Team 6'], ['Team 4', 'Team 5'], ['Team 1', 'Team 8'], ['Team 9', 'Team 7'], ['Team 10', 'Team 6'], ['Team 2', 'Team 5'], ['Team 3', 'Team 4'], ['Team 1', 'Team 7'], ['Team 8', 'Team 6'], ['Team 9', 'Team 5'], ['Team 10', 'Team 4'], ['Team 2', 'Team 3'], ['Team 1', 'Team 6'], ['Team 7', 'Team 5'], ['Team 8', 'Team 4'], ['Team 9', 'Team 3'], ['Team 10', 'Team 2'], ['Team 1', 'Team 5'], ['Team 6', 'Team 4'], ['Team 7', 'Team 3'], ['Team 8', 'Team 2'], ['Team 9', 'Team 10'], ['Team 1', 'Team 4'], ['Team 5', 'Team 3'], ['Team 6', 'Team 2'], ['Team 7', 'Team 10'], ['Team 8', 'Team 9'], ['Team 1', 'Team 3'], ['Team 4', 'Team 2'], ['Team 5', 'Team 10'], ['Team 6', 'Team 9'], ['Team 7', 'Team 8'], ['Team 1', 'Team 2'], ['Team 3', 'Team 10'], ['Team 4', 'Team 9'], ['Team 5', 'Team 8'], ['Team 6', 'Team 7']]}
+    `(
+        'returns $expected when $teams is passed to function',
+        ({ teams, expected }) => {
+            expect(fixture.createRoundRobin(teams)).toEqual(expected);
         }
     );
 });
--- src/kata.ts
+++ src/kata.ts
@@ -1,5 +1,66 @@
 export class Kata {
-    public hello(name: string): string {
-        return `Hello ${name}`;
-    }
+    roundRobinTournamentRecursive = (
+        teams: number
+    ): (number | null)[][] | undefined => {
+        const startTime = performance.now();
+
+        if (teams <= 1) {
+            return [[-1]];
+        }
+
+        const generateFixtures = (
+            teams: (number | null)[],
+            fixtures: (number | null)[][] = []
+        ): (number | null)[][] | undefined => {
+            if (teams.length === 0) {
+                console.log(
+                    `Tournament created in ${
+                        performance.now() - startTime
+                    } milliseconds.`
+                );
+                return fixtures;
+            }
+
+            for (let i = 1; i < teams.length; i++) {
+                if (teams[i] !== null) {
+                    fixtures.push([teams[0], teams[i]]);
+                }
+            }
+
+            return generateFixtures(teams.slice(1), fixtures);
+        };
+
+        const teamsArray: Array<number | null> = Array.from(
+            { length: teams },
+            (_, i) => i + 1
+        );
+        if (teams % 2 === 1) {
+            teamsArray.push(null);
+        }
+
+        return generateFixtures(teamsArray);
+    };
+
+    createRoundRobin = (numTeams: number): string[][] => {
+        const startTime = performance.now();
+        const fixtures: string[][] = [];
+        const teams: string[] = [];
+        for (let i = 1; i <= numTeams; i++) {
+            teams.push(`Team ${i}`);
+        }
+
+        const totalRounds = numTeams - 1;
+        for (let round = 0; round < totalRounds; round++) {
+            for (let i = 0; i < numTeams / 2; i++) {
+                fixtures.push([teams[i], teams[numTeams - 1 - i]]);
+            }
+            teams.splice(1, 0, teams.pop() as string);
+        }
+        console.log(
+            `Tournament created in ${
+                performance.now() - startTime
+            } milliseconds.`
+        );
+        return fixtures;
+    };
 }
