b4b908f724807b6fad3a70d5b499e978e56c829c
Brandon Schenz <bschenz@gmail.com>
2023-05-17 19:21:40 -0400

    bloom filter

--- src/kata.spec.ts
+++ src/kata.spec.ts
@@ -7,14 +7,41 @@ beforeEach(() => {
 });
 
 describe('Kata Tests', () => {
-    it.each`
-        name         | expected
-        ${'Ingage'}  | ${'Hello Ingage'}
-        ${'Brandon'} | ${'Hello Brandon'}
-    `(
-        'returns $expected when $name is passed to function',
-        ({ name, expected }) => {
-            expect(fixture.hello(name)).toEqual(expected);
-        }
-    );
+    [
+        {
+            name: 'test1',
+            given: {
+                wordsToAdd: 'apple',
+                wordToCheck: 'apple',
+            },
+            expected: true,
+        },
+        {
+            name: 'test2',
+            given: {
+                wordsToAdd: 'apple,banana',
+                wordToCheck: 'apple',
+            },
+            expected: true,
+        },
+        {
+            name: 'test3',
+            given: {
+                wordsToAdd: 'apple,banana',
+                wordToCheck: 'car',
+            },
+            expected: false,
+        },
+    ].forEach(({ name, given, expected }) => {
+        it(name, () => {
+            const bitArray: number[] = [];
+            given.wordsToAdd.split(',').forEach((word) => {
+                fixture.add(word, bitArray);
+            });
+
+            expect(fixture.isPresent(given.wordToCheck, bitArray)).toBe(
+                expected
+            );
+        });
+    });
 });
--- src/kata.ts
+++ src/kata.ts
@@ -1,5 +1,30 @@
 export class Kata {
-    public hello(name: string): string {
-        return `Hello ${name}`;
-    }
+    add = (word: string, bitArray: number[]): void => {
+        const hash1 = this.hash(word, 1);
+        const hash2 = this.hash(word, 2);
+        const hash3 = this.hash(word, 3);
+
+        bitArray[hash1] = 1;
+        bitArray[hash2] = 1;
+        bitArray[hash3] = 1;
+    };
+
+    hash = (key: string, seed: number): number => {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash = seed * (hash + key.charCodeAt(i));
+        }
+        return hash;
+    };
+
+    isPresent = (word: string, bitArray: number[]): boolean => {
+        const hash1 = this.hash(word, 1);
+        const hash2 = this.hash(word, 2);
+        const hash3 = this.hash(word, 3);
+
+        if (bitArray[hash1] === undefined) return false;
+        if (bitArray[hash2] === undefined) return false;
+        if (bitArray[hash3] === undefined) return false;
+        return true;
+    };
 }
