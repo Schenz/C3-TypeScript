d5e573b26c31c9e2175e08a8421393db0aa66eaa
Brandon Schenz <bschenz@gmail.com>
2023-09-20 19:57:12 -0400

    map reduce

--- src/kata.spec.ts
+++ src/kata.spec.ts
@@ -1,20 +1,68 @@
-import { Kata } from './kata';
+import { it, describe, expect } from '@jest/globals';
+import { Student, StudentTracker } from './kata';
 
-let fixture: Kata;
+describe('Student Class', () => {
+    it('should calculate the average grade correctly', () => {
+        const mikey = new Student('Mikey', [85, 90, 78]);
+        expect(mikey.averageGrade).toBeCloseTo(84.33, 2);
+    });
 
-beforeEach(() => {
-    fixture = new Kata();
+    it('should calculate the average grade correctly when no grades', () => {
+        const mikey = new Student('Mikey', []);
+        expect(mikey.averageGrade).toBeCloseTo(0, 2);
+    });
+
+    it('should return the correct letter grade', () => {
+        const mikey = new Student('Mikey', [95, 92, 98]);
+        const brand = new Student('Brand', [80, 88, 76]);
+        const chunk = new Student('Chunk', [75, 70, 68]);
+        const mouth = new Student('Mouth', [65, 61, 58]);
+        const troy = new Student('Troy', [55, 62, 59]);
+
+        expect(mikey.letterGrade).toBe('A');
+        expect(brand.letterGrade).toBe('B');
+        expect(chunk.letterGrade).toBe('C');
+        expect(mouth.letterGrade).toBe('D');
+        expect(troy.letterGrade).toBe('F');
+    });
 });
 
-describe('Kata Tests', () => {
-    it.each`
-        name         | expected
-        ${'Ingage'}  | ${'Hello Ingage'}
-        ${'Brandon'} | ${'Hello Brandon'}
-    `(
-        'returns $expected when $name is passed to function',
-        ({ name, expected }) => {
-            expect(fixture.hello(name)).toEqual(expected);
-        }
-    );
+describe('StudentTracker Class', () => {
+    it('should add a student to the tracker', () => {
+        const tracker = new StudentTracker();
+        tracker.addStudent('Andy', [92, 88, 95]);
+        expect(tracker.students.length).toBe(1);
+    });
+
+    it('should get students with a specific letter grade', () => {
+        const tracker = new StudentTracker();
+        const mikey = tracker.addStudent('Mikey', [95, 92, 98]);
+        const chunk = tracker.addStudent('Chunk', [75, 70, 68]);
+        const mouth = tracker.addStudent('Mouth', [65, 61, 58]);
+        const brand = tracker.addStudent('Brand', [85, 82, 88]);
+        const stef = tracker.addStudent('Stef', [91, 89, 93]);
+        const troy = tracker.addStudent('Troy', [55, 62, 59]);
+
+        const gradeAStudents = tracker.getStudentsWithLetterGrade('A');
+        const gradeBStudents = tracker.getStudentsWithLetterGrade('B');
+        const gradeCStudents = tracker.getStudentsWithLetterGrade('C');
+        const gradeDStudents = tracker.getStudentsWithLetterGrade('D');
+        const gradeFStudents = tracker.getStudentsWithLetterGrade('F');
+
+        expect(gradeAStudents.length).toBe(2);
+        expect(gradeAStudents).toContain(mikey);
+        expect(gradeAStudents).toContain(stef);
+
+        expect(gradeBStudents.length).toBe(1);
+        expect(gradeBStudents).toContain(brand);
+
+        expect(gradeCStudents.length).toBe(1);
+        expect(gradeCStudents).toContain(chunk);
+
+        expect(gradeDStudents.length).toBe(1);
+        expect(gradeDStudents).toContain(mouth);
+
+        expect(gradeFStudents.length).toBe(1);
+        expect(gradeFStudents).toContain(troy);
+    });
 });
--- src/kata.ts
+++ src/kata.ts
@@ -1,5 +1,53 @@
-export class Kata {
-    public hello(name: string): string {
-        return `Hello ${name}`;
+export class Student {
+    name: string;
+    grades: number[];
+
+    constructor(name: string, grades: number[]) {
+        this.name = name;
+        this.grades = grades;
     }
+
+    get averageGrade(): number {
+        if (this.grades.length === 0) {
+            return 0;
+        }
+
+        return (
+            this.grades.reduce((accumulator, grade) => (accumulator += grade)) /
+            this.grades.length
+        );
+    }
+
+    get letterGrade(): string {
+        const averageGrade = this.averageGrade;
+        if (averageGrade >= 90) {
+            return 'A';
+        } else if (averageGrade >= 80) {
+            return 'B';
+        } else if (averageGrade >= 70) {
+            return 'C';
+        } else if (averageGrade >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+}
+
+export class StudentTracker {
+    students: Student[];
+
+    constructor() {
+        this.students = [];
+    }
+
+    addStudent(name: string, grades: number[]): Student {
+        const student = new Student(name, grades);
+        this.students.push(student);
+
+        return student;
+    }
+
+    getStudentsWithLetterGrade = (letterGrade: string): Student[] =>
+        this.students.filter((student) => student.letterGrade === letterGrade);
 }
