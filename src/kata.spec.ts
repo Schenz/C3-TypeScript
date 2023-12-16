15ca2aa0999546330501eaa018194c532ba9a928
Brandon Schenz <bschenz@gmail.com>
2023-12-16 12:39:46 -0500

    commit

--- src/kata.spec.ts
+++ src/kata.spec.ts
@@ -1,3 +1,4 @@
+/* cSpell:disable */
 import { Kata } from './kata';
 
 let fixture: Kata;
@@ -8,13 +9,24 @@ beforeEach(() => {
 
 describe('Kata Tests', () => {
     it.each`
-        name         | expected
-        ${'Ingage'}  | ${'Hello Ingage'}
-        ${'Brandon'} | ${'Hello Brandon'}
+        name                                       | expected
+        ${'hello world!'}                          | ${[[1, 'h'], [1, 'e'], [2, 'l'], [1, 'o'], [1, ' '], [1, 'w'], [1, 'o'], [1, 'r'], [1, 'l'], [1, 'd'], [1, '!']]}
+        ${'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbb'} | ${[[34, 'a'], [3, 'b']]}
     `(
         'returns $expected when $name is passed to function',
         ({ name, expected }) => {
-            expect(fixture.hello(name)).toEqual(expected);
+            expect(fixture.runLengthEncoding(name)).toEqual(expected);
+        }
+    );
+
+    it.each`
+        name                                       | expected
+        ${'hello world!'}                          | ${[[1, 'h'], [1, 'e'], [2, 'l'], [1, 'o'], [1, ' '], [1, 'w'], [1, 'o'], [1, 'r'], [1, 'l'], [1, 'd'], [1, '!']]}
+        ${'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbb'} | ${[[34, 'a'], [3, 'b']]}
+    `(
+        'returns $expected when $name is passed to function',
+        ({ name, expected }) => {
+            expect(fixture.runLengthEncodingFunctional(name)).toEqual(expected);
         }
     );
 });
--- src/kata.ts
+++ src/kata.ts
@@ -1,5 +1,52 @@
 export class Kata {
-    public hello(name: string): string {
-        return `Hello ${name}`;
-    }
+    runLengthEncoding = (str: string): Array<[number, string]> => {
+        if (!str || str.length === 0) {
+            throw new Error('The input string cannot be null or empty.');
+        }
+
+        const result: Array<[number, string]> = [];
+
+        let currentChar = str[0];
+        let count = 1;
+
+        for (let i = 1; i < str.length; i++) {
+            if (str[i] === currentChar) {
+                count++;
+            } else {
+                result.push([count, currentChar]);
+                currentChar = str[i];
+                count = 1;
+            }
+        }
+
+        result.push([count, currentChar]);
+
+        return result;
+    };
+
+    runLengthEncodingFunctional = (str: string): Array<[number, string]> => {
+        // Check if the input string is valid
+        if (!str || str.length === 0) {
+            throw new Error('The input string cannot be null or empty.');
+        }
+        const result: Array<[number, string]> = [];
+        let currentChar = '';
+        let count = 0;
+
+        str.split('').forEach((char, index) => {
+            if (char === currentChar) {
+                count++;
+            } else {
+                if (index !== 0) {
+                    result.push([count, currentChar]);
+                }
+                currentChar = char;
+                count = 1;
+            }
+        });
+
+        result.push([count, currentChar]);
+
+        return result;
+    };
 }
